34ab1637ade0fc4ec9cb6ff58004e567ff15404d
SAD124 <sadiaumer368@gmail.com>
2024-12-18 06:37:59 -0800

    form

---
+++ src/form/Signup.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <form action="">
+        <h1>Enter your details</h1>
+        <div>
+            <label htmlFor="">Name</label>
+            <input type="text" name="" id="" />
+        </div>
+        <div>
+            <label htmlFor="">Email</label>
+            <input type="email" name="" id="" />
+        </div>
+        <div>
+            <label htmlFor="">Password</label>
+            <input type="password" name="" id="" />
+        </div>
+        <div>
+            <label htmlFor="">Confirm Password</label>
+            <input type="password" name="" id="" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Signup
